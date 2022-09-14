f67723410a3eff838269a11df96734de71564a8c
bnilesh8432 <bhoinilesh01@gmail.com.com>
2022-09-14 12:15:55 +0530

    create intern

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,8 +1,58 @@
 const InternModel = require("../models/internModel")
+const CollegeModel = require("../models/collegeModel")
+const { isValid,isValidEmail,isValidMobile} = require("../validation/validation")
 
 const createIntern = async (req, res) => {
     try {
-        console.log(req.body);
+        const data = req.body;
+        if(Object.keys(data).length === 0){
+            return res.status(400).send({ status: false, message: "Provide all fields..!!" })
+        }
+
+        if(!isValid(data.name)){
+            return res.status(400).send({ status: false, message: "Name is required..!!" })
+        }
+        data.name = data.name.trim()
+
+        if(!isValid(data.email)){
+            return res.status(400).send({ status: false, message: "Email is required..!!" })
+        }
+        data.email = data.email.trim()
+        if(!isValidEmail(data.email)){
+            return res.status(400).send({ status: false, message: "Plz enter valid email..!!" })
+        }
+        const checkDuplicateEmail = await InternModel.findOne({email : data.email})
+        if(checkDuplicateEmail){
+            return res.status(400).send({ status: false, message: "Email is already exist..!!" })
+        }
+
+        if(!isValid(data.mobile)){
+            return res.status(400).send({ status: false, message: "Mobile No is required..!!" })
+        }
+        data.mobile = data.mobile.trim()
+        if(!isValidMobile(data.mobile)){
+            return res.status(400).send({ status: false, message: "Plz enter valid mobile no..!!" })
+        }
+        const checkDuplicateMobile = await InternModel.findOne({mobile : data.mobile})
+        if(checkDuplicateMobile){
+            return res.status(400).send({ status: false, message: "Mobile No is already exist..!!" })
+        }
+
+        if(!isValid(data.collegeName)){
+            return res.status(400).send({ status: false, message: "College Name is required..!!" })
+        }
+        data.collegeName = data.collegeName.trim()
+
+        const checkCollege = await CollegeModel.findOne({$or : [{name : data.collegeName },{fullName : data.collegeName}]})
+
+        if(!checkCollege){
+            return res.status(404).send({ status: false, message: "College Name is not found..!!" })
+        }
+
+        data.collegeId = checkCollege['_id'];
+
+        const saveData = await InternModel.create(data);
+        return res.status(201).send({ status: true, data: saveData })
     } catch (error) {
         res.status(500).send({ status: false, msg: error })
     }
---
+++ src/validation/validation.js
@@ -0,0 +1,24 @@
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+};
+
+const isValidEmail = function (value) {
+    let emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+    if (emailRegex.test(value)) return true;
+  };
+
+  const isValidMobile = function (value) {
+    let mobileRegex =
+      /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
+      
+    if (mobileRegex.test(value)) return true;
+  };
+
+module.exports ={
+    isValid,
+    isValidEmail,
+    isValidMobile
+}
